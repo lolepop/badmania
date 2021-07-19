aa65b2de1540b48334a472a1d28dc3519b5e1652
Lolepopie <8401103+lolepop@users.noreply.github.com>
2021-07-19 16:21:33 +0800

    audio and ln

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css"
 import * as THREE from "three";
-import osuMapUrl from "./map.osu?raw"
+import osuMapUrl from "./map1.osu?raw"
 import parser from "osu-parser";
 import * as Stats from "stats.js";
 import { Howl, Howler } from 'howler';
@@ -30,67 +30,127 @@ function main()
     console.log(camera);
 
     const boxSize = 0.2;
-    const geometry = new THREE.BoxGeometry(boxSize, boxSize * .3, boxSize);
-    geometry.translate(-boxSize / 2, boxSize * .3 / 2, 0);
+    const geometry = new THREE.BoxGeometry(boxSize, boxSize * .3, 1);
+    geometry.translate(0, boxSize * .3 / 2, 0);
+    
+    const lnGeometry = new THREE.BoxGeometry(boxSize * .75, 1, 1);
+    lnGeometry.translate(0, 1/2, 0);
+    
     const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
 
     renderer.render(scene, camera);
 
-    let speed = .003;
+    let speed = .0035;
+    const keys = 4;
     let updateTime = -1;
     let time = 0;
 
     const osuMap = parser.parseContent(osuMapUrl);
     console.log(osuMap);
 
-    const hitObjects = osuMap.hitObjects.map(o => Object.assign(new THREE.Mesh(geometry, material), {
-        lane: Math.trunc(o.position[0] * 4 / 512),
-        startTime: o.startTime
-    }));
+    // const hitObjects = osuMap.hitObjects.map(o => Object.assign(new THREE.Mesh(geometry, material), {
+    //     lane: Math.trunc(o.position[0] * keys / 512),
+    //     startTime: o.startTime
+    // }));
 
-    hitObjects.forEach(o => {
-        o.position.z = -1;
-        o.position.x = o.lane / 4;
-        scene.add(o);
+    const hitObjects = osuMap.hitObjects.reduce((acc, o, i) => {
+        const lane = Math.trunc(o.position[0] * keys / 512);
+        acc.laneObjects[lane] = acc.laneObjects[lane] || [];
+        const currLane = acc.laneObjects[lane];
+
+        if (o.objectName !== "circle")
+        {
+            if (acc.lnState[lane])
+            {
+                const lnMesh = new THREE.Mesh(lnGeometry, material);
+                lnMesh.position.z = -1;
+                lnMesh.position.x = lane / 4;
+                
+                Object.assign(acc.laneObjects[lane][currLane.length - 1], {
+                    lnMesh,
+                    endTime: o.startTime
+                });
+
+                acc.lnState[lane] = !acc.lnState[lane];
+                return acc;
+            }
+            acc.lnState[lane] = !acc.lnState[lane];
+        }
+
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.z = -1;
+        mesh.position.x = lane / 4;
+
+        const meshObj = {
+            mesh,
+            lane,
+            startTime: o.startTime
+        };
+
+        acc.laneObjects[lane].push(meshObj);
+        return acc;
+    }, {
+        laneObjects: [],
+        lnState: Array.apply(false, Array(keys))
+    }).laneObjects;
+
+    console.log(hitObjects);
+    // return;
+
+    hitObjects.forEach(l => {
+        l.forEach(o => {
+            scene.add(o.mesh);
+            if ("endTime" in o)
+                scene.add(o.lnMesh);
+        });
     });
 
     const audio = new Howl({
-        src: ["song.mp3"]
+        src: ["song1.mp3"],
+        volume: 0.5
     });
 
     function render()
     {
         stats.begin();
-        
+
         const nowTime = new Date().getTime();
         if (updateTime > 0)
             time += nowTime - updateTime;
         updateTime = nowTime;
 
         const error = time - audio.seek() * 1000;
-        if (error > 4)
+        if (Math.abs(error) > 4)
         {
             console.log(`error over threshold: ${error}`);
             time = audio.seek() * 1000;
         }
 
-        function getPos(startTime)
+        const getPos = startTime => (startTime - time) * speed - 1;
+
+        for (const l of hitObjects)
         {
-            return (startTime - time) * speed - 1;
+            for (const o of l)
+            {
+                const startPos = getPos(o.startTime);
+                o.mesh.position.y = startPos;
+
+                if ("endTime" in o)
+                {
+                    const endPos = getPos(o.endTime);
+                    o.lnMesh.scale.y = (endPos - startPos) / 2 - (boxSize * 0.3);
+                    o.lnMesh.position.y = startPos + (boxSize * 0.3);
+                }
+                // if (o.mesh.position.y < -1.0001) 
+                // {
+                //     console.log(o);
+                //     return;
+                // }
+            }
         }
-        
-        for (const o of hitObjects)
-        {
-            o.position.y = getPos(o.startTime);
-            // if (o.position.y < -1.0001) 
-            // {
-                // console.log(o);
-            //     return;
-            // }
-        }
-        
+
         renderer.render(scene, camera);
-        
+
         stats.end();
         requestAnimationFrame(render);
     }
